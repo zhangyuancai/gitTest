8cf390465372bdf374101cccf81c36e40d9d40ff
yuancai302278 <71876505+zhangyuancai@users.noreply.github.com>
2021-12-08 20:47:37 +0800

    第八次提交

--- src/main/java/zhang/yaun/git.java
+++ src/main/java/zhang/yaun/git.java
@@ -9,5 +9,6 @@ public class git {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("pull");
+        System.out.println("pull222");
     }
 }
